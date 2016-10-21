a3048307ba5b3f83fae35485b4dc696c2ff6d29c
ghurabah93 <ghurabah93@hotmail.com>
2016-10-21 20:06:00 +0800

    Update SSUSchema290916.sql

--- crowd_funding/SSUSchema290916.sql
+++ crowd_funding/SSUSchema290916.sql
@@ -31,8 +31,9 @@ name VARCHAR(256) NOT NULL UNIQUE,
 description VARCHAR(256),
 start_date DATE,
 end_date DATE,
-amount NUMERIC NOT NULL DEFAULT 0.0
-raised NUMERIC NOT NULL DEFAULT 0.0
+amount NUMERIC NOT NULL DEFAULT 0.0,
+raised NUMERIC NOT NULL DEFAULT 0.0,
+number_of_contributer INT DEFAULT 0
 );
 
 CREATE TABLE bookmark (
